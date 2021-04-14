7beefe67733733935686c1f83c13f3d3f54344ba
Felix Moerman <felix.moerman@hotmail.com>
2021-04-14 17:51:49 +0200

    Moved up listing all videos

--- R/Locate_and_measure_particles.R
+++ R/Locate_and_measure_particles.R
@@ -29,6 +29,10 @@ locate_and_measure_particles <- function(to.data, raw.video.folder, particle.dat
   video.dir <- paste(to.data, raw.video.folder, sep = "")
   
   ##Determine the maximum number of processess that can be run in parallel
+  #List all video files
+  video.files <- list.files(path = video.dir, pattern = paste("\\.", video.format, sep="")) #Make a dataframe containing the full video files
+  video.files <- paste(video.dir, video.files, sep="/")
+  video.files.df <- as.data.frame(video.files)
   #Filter the df to get a list of the videos that will be analyzed by this processor core
   video.files.filt <- as.character(video.files.df[which(video.files.df$process==process_ID), "video.files"])
   #Ensure that the assigned number of cores is not greater than the number of cores of the machine - 1
@@ -36,7 +40,6 @@ locate_and_measure_particles <- function(to.data, raw.video.folder, particle.dat
   #Make sure enough memory (10000 Mb) is reserved for every parallel process
   processes <- max(min(max.cores, memory %/% memory_per_identifier), 1)
   #Ensure that every core needs to analyze at least 5 videos (otherwise parallellization does not yield enough time benefit)
-  video.files <- list.files(path = video.dir, pattern = paste("\\.", video.format, sep=""))
   min(processes, floor(length(as.character(video.files.df[which(video.files.df$process==process_ID), "video.files"]))/5))
   
   #In Windows, or if only limited computational power, use the unparallellized version of the analysis
@@ -78,10 +81,6 @@ locate_and_measure_particles <- function(to.data, raw.video.folder, particle.dat
     error.log <- system(cmd, intern = T)
   } else {
     #Otherwise, perform the parallellized analysis
-    
-    #Make a dataframe containing the full video files
-    video.files <- paste(video.dir, video.files, sep="/")
-    video.files.df <- as.data.frame(video.files)
     
     #Divide the videos between the processors that can be used
     video.files.df$process <- ceiling(seq(from=0.001, to=processes, length=nrow(video.files.df)))
